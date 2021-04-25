62b25d8b87c31ffee6407f0e7ac2ebcf3e13b764
Ilton Batista da Silva Júnior <xlsonplay@gmail.com>
2021-04-25 11:42:51 -0300

    Requisição SPA que faz com que as informações aparecem depois que a pagina carregou para o usuario, não comum em fazer nesse caso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-import { Header } from "../Components/Header";
+/* Primeira forma e a tradicional SPA*/
+/* Segunda forma e a tradicional SSR*/
+/* Terceira forma e a tradicional SSG*/
+
+import { useEffect } from "react"
 
 export default function Home() {
+  //Função para efeitos colaterias
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes').then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
+  
   return (
     <h1>Index</h1>
   )
