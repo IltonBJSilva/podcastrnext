ba902f3bce6073f64364af62fb8789f8332cbdfa
Ilton Batista da Silva Júnior <xlsonplay@gmail.com>
2021-04-25 11:53:00 -0300

    Conceito de SSR onde realiza uma requisição toda vez que um usuario novo entra na pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,24 @@
 /* Segunda forma e a tradicional SSR*/
 /* Terceira forma e a tradicional SSG*/
 
-import { useEffect } from "react"
 
-export default function Home() {
-  //Função para efeitos colaterias
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes').then(response => response.json())
-    .then(data => console.log(data))
-  }, [])
-  
+
+export default function Home(props) {  
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getServerSideProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return{
+    props:{
+      episodes: data,
+    }
+  }
 }
